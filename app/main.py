cdd34d7e92a74730268f2be3ba88720b23a06f9a
Matheus Bessa <mathebessamo@gmail.com>
2025-09-09 13:33:46 -0300

    Solition

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    persons = []
+    for p in people:
+        person = Person(p["name"], p["age"])
+        persons.append(person)
+    for p in people:
+        person = Person.people[p["name"]]
+        if "wife" in p and p["wife"] is not None:
+            person.wife = Person.people[p["wife"]]
+        if "husband" in p and p["husband"] is not None:
+            person.husband = Person.people[p["husband"]]
+    return persons
