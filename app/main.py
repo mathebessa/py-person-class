24bad4e38a2202ae865f3419fa94c7b8717096b0
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 18:34:54 -0300

    Tentando23

--- app/main.py
+++ app/main.py
@@ -1,44 +1,34 @@
-from typing import List, Dict, Union, Any
+from typing import List, Dict, Union
 
 
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int,
-                 wife_name: str = None, husband_name: str = None) -> None:
+    def __init__(self, name: str, age: int, wife_name: str = None, husband_name: str = None) -> None:
         self.name = name
         self.age = age
-        self.wife_name = wife_name
-        self.husband_name = husband_name
         self.wife = None
         self.husband = None
         Person.people[name] = self
 
+        if wife_name and wife_name in Person.people:
+            self.wife = Person.people[wife_name]
+        if husband_name and husband_name in Person.people:
+            self.husband = Person.people[husband_name]
+
     def set_spouse(self) -> None:
-        if self.wife_name and self.wife_name in Person.people:
-            self.wife = Person.people[self.wife_name]
-        if self.husband_name and self.husband_name in Person.people:
-            self.husband = Person.people[self.husband_name]
-
-    def __getattr__(self, name: str) -> Any:
-        if name == "wife" and self.wife is None:
-            return None  # Não lançar erro se não houver esposa
-        if name == "husband" and self.husband is None:
-            return None  # Não lançar erro se não houver marido
-        return object.__getattr__(self, name)
+        pass
 
 
-def create_person_list(
-        people_data: List[Dict[str, Union[str, int]]]) -> List[Person]:
-    person_objects = {}
+def create_person_list(people_data: List[Dict[str, Union[str, int]]]) -> List[Person]:
+    person_objects = []
     for person in people_data:
         name = person["name"]
         age = person["age"]
         wife_name = person.get("wife", None)
         husband_name = person.get("husband", None)
-        person_objects[name] = Person(name, age, wife_name, husband_name)
 
-    for person in person_objects.values():
-        person.set_spouse()
+        person_obj = Person(name, age, wife_name, husband_name)
+        person_objects.append(person_obj)
 
-    return list(person_objects.values())
+    return person_objects
