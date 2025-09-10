d390c9de3a6c3b0337f7478f1c22c52fc0a6adcd
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 12:50:33 -0300

    Tentando4

--- app/main.py
+++ app/main.py
@@ -1,24 +1,26 @@
 class Person:
-    people: dict[str, "Person"] = {}
+    people = {}
 
-    def __init__(self, name: str, age: int) -> None:
+    def __init__(self, name: str, wife_name: str = None, husband_name: str = None):
         self.name = name
-        self.age = age
         Person.people[name] = self
 
+        if wife_name is not None:
+            self.wife = Person.people[wife_name]
+        if husband_name is not None:
+            self.husband = Person.people[husband_name]
 
-def create_person_list(
-    people_data: list[dict[str, str | int | None]]
-) -> list[Person]:
-    persons = [Person(person["name"], person["age"])
-               for person in people_data]
 
-    for person_dict in people_data:
-        person_instance = Person.people[person_dict["name"]]
-        wife_name = person_dict.get("wife")
-        husband_name = person_dict.get("husband")
-        if wife_name:
-            person_instance.wife = Person.people[wife_name]
-        if husband_name:
-            person_instance.husband = Person.people[husband_name]
-    return persons
+person1 = Person("John", wife_name="Jane")
+person2 = Person("Jane", husband_name="John")
+person3 = Person("Alice")
+
+print(person1.name)
+print(person1.wife.name)
+print(person2.name)
+print(person2.husband.name)
+
+try:
+    print(person3.wife)
+except AttributeError as e:
+    print("AttributeError:", e)
