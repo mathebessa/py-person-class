7b9f18019da8b81d4420a59694dc601fcacf6138
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 16:55:30 -0300

    Tentando17

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ class Person:
     people = {}
 
     def __init__(
-        self,
-        name: str,
-        wife_name: str = None,
-        husband_name: str = None,
+            self,
+            name: str,
+            age: int,
+            wife_name: str = None,
+            husband_name: str = None,
     ) -> None:
         self.name = name
+        self.age = age
         Person.people[name] = self
 
         if wife_name is not None:
@@ -25,8 +27,15 @@ class Person:
             self.husband = Person.people[husband_name]
 
     def __str__(self) -> str:
-        return f"Person(name={self.name})"
+        return f"Person(name={self.name}, age={self.age})"
 
 
-def create_person_list(names: list) -> list:
-    return [Person(name) for name in names]
+def create_person_list(people_data: list) -> list:
+    for person_data in people_data:
+        name = person_data["name"]
+        age = person_data["age"]
+        wife_name = person_data.get("wife")
+        husband_name = person_data.get("husband")
+
+        Person(name, age, wife_name, husband_name)
+    return list(Person.people.values())
