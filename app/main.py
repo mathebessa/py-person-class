ebb65ca34da754159c5d87aa30967e8f51b1cf23
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 17:28:51 -0300

    Tentando20

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
-from typing import List, \
-    Dict, Union
+from typing import List, Dict, Union
 
 
 class Person:
     people = {}
 
-    def __init__(self, name: str,
-                 age: int, wife_name: str = None,
-                 husband_name: str = None) -> None:
+    def __init__(self, name: str, age: int,
+                 wife_name: str = None, husband_name: str = None) -> None:
         self.name = name
         self.age = age
         self.wife_name = wife_name
@@ -17,23 +15,20 @@ class Person:
         Person.people[name] = self
 
     def set_spouse(self) -> None:
-        if self.wife_name and \
-           self.wife_name in Person.people:
+        if self.wife_name and self.wife_name in Person.people:
             self.wife = Person.people[self.wife_name]
-        if self.husband_name and \
-           self.husband_name in Person.people:
+        if self.husband_name and self.husband_name in Person.people:
             self.husband = Person.people[self.husband_name]
 
-def create_person_list(people_data: \
-    List[Dict[str, Union[str, int]]]) -> List[Person]:
+
+def create_person_list(people_data: List[Dict[str, Union[str, int]]]) -> List[Person]:
     person_objects = {}
     for person in people_data:
         name = person["name"]
         age = person["age"]
         wife_name = person.get("wife", None)
         husband_name = person.get("husband", None)
-        person_objects[name] = \
-            Person(name, age, wife_name, husband_name)
+        person_objects[name] = Person(name, age, wife_name, husband_name)
 
     for person in person_objects.values():
         person.set_spouse()
