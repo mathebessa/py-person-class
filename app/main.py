2350fbe220e2b753c5e5e9a75beacc6e120424a7
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 11:09:12 -0300

    Solution8

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
-class Pessoa:
-    people: dict[str, "Pessoa"] = {}
+class Person:
+    people: dict[str, "Person"] = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Pessoa.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(
     people_data: list[dict[str, str | int | None]]
-) -> list[Pessoa]:
-    persons = [Pessoa(person["name"], person["age"])
+) -> list[Person]:
+    persons = [Person(person["name"], person["age"])
                for person in people_data]
 
     for person_dict in people_data:
-        person_instance = Pessoa.people[person_dict["name"]]
+        person_instance = Person.people[person_dict["name"]]
         wife_name = person_dict.get("wife")
         husband_name = person_dict.get("husband")
         if wife_name:
-            person_instance.wife = Pessoa.people[wife_name]
+            person_instance.wife = Person.people[wife_name]
         if husband_name:
-            person_instance.husband = Pessoa.people[husband_name]
+            person_instance.husband = Person.people[husband_name]
     return persons
