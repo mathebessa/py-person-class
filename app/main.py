1d10423e1ec0d5764cf06e5c800ddaca3aeea08d
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 19:59:47 -0300

    Tentando23

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Union, Any
+from typing import List, Dict, Union
 
 
 class Person:
@@ -15,7 +15,7 @@ class Person:
     def set_spouse(self) -> None:
         if self._wife_name:
             if self._wife_name in Person.people:
-                self._wife = Person.people[self._wife_name]
+                setattr(self, "wife", Person.people[self._wife_name])
             else:
                 raise AttributeError(
                     f"{self.name} has no wife named {self._wife_name}"
@@ -23,7 +23,7 @@ class Person:
 
         if self._husband_name:
             if self._husband_name in Person.people:
-                self._husband = Person.people[self._husband_name]
+                setattr(self, "husband", Person.people[self._husband_name])
             else:
                 raise AttributeError(
                     f"{self.name} has no husband named {self._husband_name}"
@@ -32,26 +32,6 @@ class Person:
         # Remover os atributos temporários depois da ligação
         del self._wife_name
         del self._husband_name
-
-    def __getattr__(self, attr: str) -> Any:
-        if attr == "wife" and not hasattr(self, "_wife"):
-            raise AttributeError(f"{self.name} does not have a wife")
-        elif attr == "husband" and not hasattr(self, "_husband"):
-            raise AttributeError(f"{self.name} does not have a husband")
-
-        return super().__getattr__(attr)
-
-    @property
-    def wife(self) -> "Person":
-        if hasattr(self, "_wife"):
-            return self._wife
-        raise AttributeError(f"{self.name} does not have a wife")
-
-    @property
-    def husband(self) -> "Person":
-        if hasattr(self, "_husband"):
-            return self._husband
-        raise AttributeError(f"{self.name} does not have a husband")
 
 
 def create_person_list(
