50a67d119c6bbab5622f372e30727d328d6f2224
Matheus Bessa <mathebessamo@gmail.com>
2025-09-10 15:46:35 -0300

    Tentando10

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 class Pessoa:
     pessoas = {}
 
-    def __init__(self,
-                 nome: str,
+    def __init__(self, nome: str,
                  nome_da_esposa: str = None,
                  nome_do_marido: str = None) -> None:
         self.nome = nome
@@ -10,20 +9,26 @@ class Pessoa:
 
         if nome_da_esposa is not None:
             if nome_da_esposa not in Pessoa.pessoas:
-                raise ValueError(f"A pessoa {nome_da_esposa} não foi registrada.")
+                raise ValueError(
+                    f"A pessoa {nome_da_esposa} não foi registrada."
+                )
             self.esposa = Pessoa.pessoas[nome_da_esposa]
 
         if nome_do_marido is not None:
             if nome_do_marido not in Pessoa.pessoas:
-                raise ValueError(f"A pessoa {nome_do_marido} não foi registrada.")
+                raise ValueError(
+                    f"A pessoa {nome_do_marido} não foi registrada."
+                )
             self.marido = Pessoa.pessoas[nome_do_marido]
 
     def __str__(self) -> str:
-        return f"Pessoa(nome={self.nome})"
+        return f'Pessoa(nome={self.nome})'
 
 
-pessoa1 = Pessoa("John", nome_da_esposa="Jane")
-pessoa2 = Pessoa("Jane", nome_do_marido="John")
+pessoa1 = Pessoa("John",
+                 nome_da_esposa="Jane")
+pessoa2 = Pessoa("Jane",
+                 nome_do_marido="John")
 pessoa3 = Pessoa("Alice")
 
 print(pessoa1.nome)
