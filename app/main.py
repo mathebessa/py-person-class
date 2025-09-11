75dbf297acc2aba4e5a7a6617d159df237162535
Matheus Bessa <mathebessamo@gmail.com>
2025-09-11 00:17:14 -0300

    finalizando

--- app/main.py
+++ app/main.py
@@ -4,8 +4,13 @@ from typing import List, Dict, Union
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int, wife_name: str = None,
-                 husband_name: str = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            age: int,
+            wife_name: str = None,
+            husband_name: str = None,
+    ) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
@@ -13,14 +18,11 @@ class Person:
         self._husband_name = husband_name
 
     def set_spouse(self) -> None:
-        if (
-            self._wife_name is not None
-            and self._wife_name in Person.people
-        ):
+        if self._wife_name is not None and self._wife_name in Person.people:
             setattr(self, "wife", Person.people[self._wife_name])
         if (
-            self._husband_name is not None
-            and self._husband_name in Person.people
+                self._husband_name is not None
+                and self._husband_name in Person.people
         ):
             setattr(self, "husband", Person.people[self._husband_name])
 
@@ -33,8 +35,10 @@ def create_person_list(
 ) -> List[Person]:
     person_objects = [
         Person(
-            person["name"], person["age"],
-            person.get("wife", None), person.get("husband", None)
+            person["name"],
+            person["age"],
+            person.get("wife", None),
+            person.get("husband", None),
         )
         for person in people_data
     ]
